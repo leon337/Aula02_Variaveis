b5aeaef875093912fa8a076f681a70d996fd2c5b
Leandro Carlos <leonpcsn@gmail.com>
2021-03-17 22:15:52 -0300

    Finalizando Aula 02 Variaveis

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert('Olá Mundo!');
+alert('Iniciando meu scriptJS');
+
+window.onload = function(){
+  let nome,string,number,boolean;
+
+  nome = "Leandro";
+
+  alert('Olá esse codigo é do ' + nome);
+  
+  console.log('Agora eu finalizei o codigo');
+
+  string = 'VariavelString';
+  number = 32;
+  boolean = true;
+
+  console.log(string,number,true);
+
+  
+}
